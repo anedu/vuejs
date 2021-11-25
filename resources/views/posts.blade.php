36d78e3f2192b814751fe56337fc2dfe6acca65d
anedu <annushkashop@gmail.com>
2021-11-25 16:58:49 +0900

    made front

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -3,35 +3,49 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Посты</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 	<script src="https://cdn.jsdelivr.net/npm/vue@2/dist/vue.js"></script>
 </head>
-<body>
-	<div id="app">
-		@{{ name + ' ' + surname}}
-		<button v-on:click="createPost">Создать</button>
-		<button v-on:click="deletePost">Удалить</button>
-		<button v-on:click="updatePost">Обновить</button>
-		<div class="box" style="height: 100px; width: 100px; background-color: red;"></div>
-		<div>
-			<?= csrf_field(); ?>
-			<input v-model="post.title" type="text" placeholder="Заголовок">
-			<input v-model="post.text" type="text" placeholder="Текст">
-			<button v-on:click="createPost">Создать</button>
+<body class="bg-dark text-white-50">
+	<!-- header -->
+	<div class="bg-secondary col-12" style="height: 60px;">
+		<div class="col-10 mx-auto" style="height: 60px;">
 		</div>
-		<div v-for="post in posts">
-			<h1>@{{ post.title }}</h1>
-			<p>@{{ post.text }}</p>
-			<p><span style="font-weight: bold;">ID: </span>@{{ post.id }}</p>
-			<div>
+	</div>
+	<!-- main -->
+	<div class="col-6 mx-auto">
+		<div id="app">
+			<!-- create post -->
+			<div class="col-12">
+				<h5>Создать пост</h5>
 				<?= csrf_field(); ?>
-				<button v-on:click="deletePost(post.id)">Удалить</button>
+				<div class="my-3">
+					<input class="form-control bg-dark text-white-50" v-model="post.title" type="text" placeholder="Введите заголовок">
+				</div>
+				<div class="my-3">
+					<textarea class="form-control bg-dark text-white-50" rows="3" v-model="post.text" placeholder="Введите текст"></textarea>
+				</div>
+				<div class="my-3">
+					<button class="btn btn-primary" v-on:click="createPost">Создать</button>
+				</div>
 			</div>
-			<div>
-				<?= csrf_field(); ?>
-				<input type="text" v-model="post.id">
-				<input type="text" v-model="post.title">
-				<input type="text" v-model="post.text">
-				<button v-on:click="updatePost(post.id, post.title, post.text)">Обновить</button>
+			<div v-for="post in posts" class="col-12">
+				<h4 class="my-3">@{{ post.title }}</h4>
+				<p class="my-3">@{{ post.text }}</p>
+				<p class="my-3"><span style="font-weight: bold;">ID: </span>@{{ post.id }}</p>
+				<!-- delete -->
+				<div  class="my-3">
+					<?= csrf_field(); ?>
+					<button class="btn btn-danger" v-on:click="deletePost(post.id)">Удалить</button>
+				</div>
+				<!-- update -->
+				<div>
+					<?= csrf_field(); ?>
+					<input type="hidden" v-model="post.id">
+					<input type="text" class="form-control my-3 bg-dark text-white-50" v-model="post.title">
+					<textarea class="form-control my-3 bg-dark text-white-50" rows="3" placeholder="Введите текст" v-model="post.text"></textarea>
+					<button class="btn btn-success mb-3" v-on:click="updatePost(post.id, post.title, post.text)">Обновить</button>
+				</div>
 			</div>
 		</div>
 	</div>
@@ -40,8 +54,6 @@
 		let app = new Vue({
 			el: '#app',
 			data: {
-				name: 'Anya',
-				surname: 'Arutkina',
 				posts: [],
 				post: {
 					id: '',
@@ -54,56 +66,43 @@
 			},
 			methods: {
 				getPosts: function() {
-					alert('Mounted вызывается!');
+					console.log('Mounted вызывается!');
 					axios.get('/show')
 						.then(response => {
 							this.posts = response.data;
-							console.log(this.posts);
 							console.log('Mounted вызывается!');
 						})
 						.catch(error => console.log(error));			
 				},
 				createPost: function() {
 					console.log('Функция createPost вызывается!');
-					let box = document.querySelector('.box');
-					box.style.backgroundColor = 'green';
-
 					axios.post('/insert', {
 							title: this.post.title,
 							text: this.post.text
 						})
 						.then(response => {
-							console.log(response);
 							this.getPosts();
 						})
 						.catch(error => console.log(error));
 				},
 				deletePost: function(id) {
 					console.log('Функция deletePost вызывается!');
-					let box = document.querySelector('.box');
-					box.style.backgroundColor = 'orange';
-
 					axios.post('/delete', {
 							id: id
 						})
 						.then(response => {
-							console.log(response);
 							this.getPosts();
 						})
 						.catch(error => console.log(error));
 				},
 				updatePost: function(id, title, text) {
 					console.log('Функция updatePost вызывается!');
-					let box = document.querySelector('.box');
-					box.style.backgroundColor = 'yellow';
-
 					axios.post('/update', {
 							id: id,
 							title: title,
 							text: text
 						})
 						.then(response => {
-							console.log(response);
 							console.log(this.post.id + ' ' + this.post.title + ' ' + this.post.text);
 							this.getPosts();
 						})
